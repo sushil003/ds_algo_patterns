9f3761059a123e56d6200fd75c01c9617def7506
Sushil <sushilzende1@gmail.com>
2021-01-29 18:37:06 -0600

    two pointer problems

---
+++ src/com/patterns/twopointers/PairWithTargetSum.java
@@ -0,0 +1,34 @@
+package com.patterns.twopointers;
+
+/**
+ * Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+ * Input: [1, 2, 3, 4, 6], target=6
+ * Output: [1, 3]
+ * Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+ * Time complexity : O(N)
+ * Space complexity : O(1)
+ */
+public class PairWithTargetSum {
+
+    public static void main(String[] args) {
+        int[] result = PairWithTargetSum.search(new int[]{1, 2, 3, 4, 6}, 6);
+        System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+        result = PairWithTargetSum.search(new int[]{2, 5, 9, 11}, 11);
+        System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+    }
+
+    public static int[] search(int[] arr, int targetSum) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int currentSum = arr[left] + arr[right];
+            if (currentSum == targetSum)
+                return new int[]{left, right}; // found the pair
+
+            if (targetSum > currentSum)
+                left++; // we need a pair with a bigger sum
+            else
+                right--; // we need a pair with a smaller sum
+        }
+        return new int[]{-1, -1};
+    }
+}
