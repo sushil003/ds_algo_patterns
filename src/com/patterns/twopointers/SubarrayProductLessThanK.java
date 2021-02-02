c9eadcf9c1f98b682458dd4eec891986493dc58e
Sushil <sushilzende1@gmail.com>
2021-02-02 14:39:33 -0600

    SubArray Product

---
+++ src/com/patterns/twopointers/SubarrayProductLessThanK.java
@@ -0,0 +1,41 @@
+package com.patterns.twopointers;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given an array with positive numbers and a target number, find all of its contiguous subarrays whose product is less
+ * than the target number.
+ * Input: [2, 5, 3, 10], target=30
+ * Output: [2], [5], [2, 5], [3], [5, 3], [10]
+ * Explanation: There are six contiguous subarrays whose product is less than the target.
+ * Time complexity : O(N^3)
+ * Space complexity : O(N^3) space for sorting
+ */
+public class SubarrayProductLessThanK {
+
+    public static void main(String[] args) {
+        System.out.println(SubarrayProductLessThanK.findSubarrays(new int[]{2, 5, 3, 10}, 30));
+        System.out.println(SubarrayProductLessThanK.findSubarrays(new int[]{8, 2, 6, 5}, 50));
+    }
+
+    public static List<List<Integer>> findSubarrays(int[] arr, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int product = 1, left = 0;
+        for (int right = 0; right < arr.length; right++) {
+            product *= arr[right];
+            while (product >= target && left < arr.length)
+                product /= arr[left++];
+            // since the product of all numbers from left to right is less than the target therefore,
+            // all subarrays from left to right will have a product less than the target too; to avoid
+            // duplicates, we will start with a subarray containing only arr[right] and then extend it
+            List<Integer> tempList = new LinkedList<>();
+            for (int i = right; i >= left; i--) {
+                tempList.add(0, arr[i]);
+                result.add(new ArrayList<>(tempList));
+            }
+        }
+        return result;
+    }
+}
